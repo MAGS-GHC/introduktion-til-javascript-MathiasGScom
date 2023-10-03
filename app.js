af58e721ac980bae56bf33b7b05a9e7c0330df35
MAGS <103378236+MathiasGScom@users.noreply.github.com>
2023-10-03 11:31:32 +0200

    Lavet opgave 1.1 og 1.2

--- app.js
+++ app.js
@@ -4,4 +4,11 @@ console.log(
   "Opgave 1.1 - Lav et program, som skriver “Hello World” i konsollen"
 );
 //Lav jeres svar her
-console.log("XX");
+console.log("Hello World");
+
+console.log(
+  "Opgave 2.2 Lav et program, som skriver “Hello” og på næste linje skriver “World” i konsollen"
+);
+
+console.log("Hello \nWorld");
+console.log("World ");
